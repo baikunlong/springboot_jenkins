22ed89abf9168b225ff926a0750af62db763a0f5
baikunlong <1262198701@qq.com>
2020-10-04 11:54:31 +0800

    小号提交测试

--- src/main/java/top/baikunlong/jenkins/Controller/TestController.java
+++ src/main/java/top/baikunlong/jenkins/Controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    String msg="小号测试下";
     @GetMapping("/test")
     public String test(){
         return "这是测试jenkins自动化部署的项目。端口-->8989，再来试试push成功了吗333,使用docker方式启动！！！";
