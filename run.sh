661044ebe13bb9307b720f118c8cfca2ded520aa
baikunlong <1262198701@qq.com>
2020-10-04 10:05:41 +0800

    修改了run.sh和build.sh的权限为可执行

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 docker ps -a
-
+# 停止容器
 docker stop springboot_jenkins_8989
+# 删除容器
 docker rm -f springboot_jenkins_8989
+# 创建容器
 docker run -d --name springboot_jenkins_8989 -p 8989:8989 baikunlong.top/springboot_jenkins:1.0
-
+# 查看docker进程
 docker ps -a
